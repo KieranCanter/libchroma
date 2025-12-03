9f502083c02d20928ef3c006e9a9934dfa58a818
Kieran Canter <canter.kieran@gmail.com>
2025-12-03 16:36:48 -0500

    added hsi, hwb, yiq, yuv, changed f64 to f32

--- src/Color.zig
+++ src/Color.zig
@@ -2,46 +2,42 @@ const std = @import("std");
 
 const Cmyk = @import("color-spaces/Cmyk.zig").Cmyk;
 const Hex = @import("color-spaces/Hex.zig").Hex;
+const Hsi = @import("color-spaces/Hsi.zig").Hsi;
 const Hsl = @import("color-spaces/Hsl.zig").Hsl;
 const Hsv = @import("color-spaces/Hsv.zig").Hsv;
-const LinearRgb = @import("color-spaces/LinearRgb.zig").LinearRgb;
+const Hwb = @import("color-spaces/Hwb.zig").Hwb;
 const Srgb = @import("color-spaces/Srgb.zig").Srgb;
 const Xyz = @import("color-spaces/Xyz.zig").Xyz;
+const Yiq = @import("color-spaces/Yiq.zig").Yiq;
+const Yuv = @import("color-spaces/Yuv.zig").Yuv;
 
 pub const ColorSpace = enum {
     cmyk,
     hex,
+    hsi,
     hsl,
     hsv,
-    linearRgb,
+    hwb,
     srgb,
     xyz,
+    yiq,
+    yuv,
 };
 
-pub const ColorValue = union(ColorSpace) {
+pub const Color= union(ColorSpace) {
     cmyk: Cmyk,
     hex: Hex,
+    hsi: Hsi,
     hsl: Hsl,
     hsv: Hsv,
-    linearRgb: LinearRgb,
+    hwb: Hwb,
     srgb: Srgb,
     xyz: Xyz,
+    yiq: Yiq,
+    yuv: Yuv,
 };
 
-pub const Color = struct {
-    value: ColorValue,
-    alpha: f64,
+pub const AlphaColor = struct {
+    value: Color,
+    alpha: f32,
 };
-
-pub const Canonical = struct {
-    value: Xyz,
-    alpha: f64,
-};
-
-pub fn toCanonical(color: Color) Canonical {
-    @compileLog("TODO: Implement `toCanonical()");
-}
-
-pub fn fromCanonical(canon: Canonical) Color {
-    @compileLog("TODO: Implement `fromCanonical()");
-}
