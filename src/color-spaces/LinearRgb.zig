36e70f2d5702bc634738c6904bcdb27925798ac6
Kieran Canter <canter.kieran@gmail.com>
2025-12-03 16:39:42 -0500

    removed LinearRgb, merged into sRGB as LinearSrgb; will use individualized Linear*rgb types for future RGB types

--- src/color-spaces/LinearRgb.zig
+++
@@ -1,8 +0,0 @@
-pub const LinearRgb = struct {
-    r: f64,
-    g: f64,
-    b: f64,
-
-    // Matrices for various RGB <-> XYZ conversions with linearized RGB values:
-    // http://www.brucelindbloom.com/index.html?Eqn_RGB_XYZ_Matrix.html
-};
