4f5f4ab9c1ffea47ac516944f4a93b5363f36262
Kieran Canter <canter.kieran@gmail.com>
2025-12-22 14:48:18 -0500

    removed main executable

--- src/main.zig
+++
@@ -1,7 +0,0 @@
-const std = @import("std");
-const chroma = @import("libchroma");
-
-pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("libchroma 󰏘", .{});
-}
