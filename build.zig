94799864c41fc6981fe7e3cfb926969a7a551d29
Kieran Canter <canter.kieran@gmail.com>
2025-12-22 14:51:06 -0500

    removed executable related build steps

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
 
     const mod = b.addModule("libchroma", .{
-        .root_source_file = b.path("src/root.zig"),
+        .root_source_file = b.path("src/lib.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -26,45 +26,13 @@ pub fn build(b: *std.Build) !void {
     b.installArtifact(staticLib);
     b.installArtifact(dynLib);
 
-    const exe = b.addExecutable(.{
-        .name = "chroma-cli",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-            .imports = &.{
-                .{ .name = "libchroma", .module = mod },
-            },
-        }),
-    });
-    b.installArtifact(exe);
-
-    const run_step = b.step("run", "Run the app");
-
-    const run_cmd = b.addRunArtifact(exe);
-    run_step.dependOn(&run_cmd.step);
-
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
     const mod_tests = b.addTest(.{
         .root_module = mod,
     });
-
     const run_mod_tests = b.addRunArtifact(mod_tests);
-
-    const exe_tests = b.addTest(.{
-        .root_module = exe.root_module,
-    });
-
-    const run_exe_tests = b.addRunArtifact(exe_tests);
 
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_mod_tests.step);
-    test_step.dependOn(&run_exe_tests.step);
 }
 
 fn incrementBuildNumber(b: *std.Build) !std.SemanticVersion {
