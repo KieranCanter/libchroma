fd514c3b8f3a7d6802b59bd92ae9d35e3c135d8f
Kieran Canter <canter.kieran@gmail.com>
2025-12-23 15:50:33 -0500

    updated cmyk to follow color interface contract

--- src/color_space/cmyk.zig
+++ src/color_space/cmyk.zig
@@ -1,3 +1,5 @@
+const assertFloatType = @import("../validation.zig").assertFloatType;
+
 const Srgb = @import("srgb.zig").Srgb;
 const Xyz = @import("xyz.zig").Xyz;
 
@@ -8,20 +10,27 @@ const Xyz = @import("xyz.zig").Xyz;
 /// y: yellow value in [0.0, 1.0]
 /// k: black value in [0.0, 1.0]
 pub fn Cmyk(comptime T: type) type {
+    assertFloatType(T);
+
     return struct {
         const Self = @This();
+        pub const Backing = T;
 
         c: T,
         m: T,
         y: T,
         k: T,
 
-        pub fn toXyz(self: Self) @TypeOf(Xyz(T)) {
+        pub fn init(c: T, m: T, y: T, k: T) Self {
+            return .{ .c = c, .m = m, .y = y, .k = k };
+        }
+
+        pub fn toXyz(self: Self) Xyz(T) {
             return self.toSrgb().toXyz();
         }
 
-        pub fn fromXyz(xyz: @TypeOf(Xyz(T))) Self {
-            return xyz.toSrgb().toCmyk();
+        pub fn fromXyz(xyz: anytype) Self {
+            return Srgb(T).fromXyz(xyz).toCmyk();
         }
 
         // Formula for CMYK -> sRGB conversion:
